bef77899b497c5e00b43f5ae9d6d90c408903310
KazusaNakagawa <kazusanakagawa@gmail.com>
2022-04-12 08:12:37 +0900

    feat: get user/me API

--- sample-project/axios.js
+++ sample-project/axios.js
@@ -3,7 +3,8 @@
 
 const axios = require("axios")
 const url = 'http://0.0.0.0:8000'
-const AUTH_TOKEN = ''
+const AUTH_TOKEN = null
+
 
 async function getUsers() {
   await axios({
@@ -11,16 +12,13 @@ async function getUsers() {
     url: `${url}/users`,
   })
   .then(function (res) {
-    console.log({data: res.data})
-    console.log({status: res.status})
-    console.log(response.data);
-    console.log(response.status);
-    console.log(response.statusText);
-    console.log(response.headers);
-    console.log(response.config);
+    console.log({ data: res.data })
+    console.log({ status: res.status })
+    console.log({ statusText: res.statusText });
+    console.log({ headers: res.headers });
+    console.log({ config: res.config });
   })
   .catch(function (err) {
-    // alert(error)
     console.log({ errorRes: err.response })
     console.log({ errorStatus: err.status })
     console.log({ errorData: err.data })
@@ -28,9 +26,9 @@ async function getUsers() {
 }
 
 async function postApi(endpoint, username, password) {
-  // axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
   // axios.defaults.xsrfCookieName = 'csrftoken' // ←ココと
   // axios.defaults.xsrfHeaderName = "X-CSRFTOKEN" // ←ココに追加しました
+  // axios.defaults.headers.common['Authorization'] = `Bearer ${AUTH_TOKEN}`
   await axios({
     method: 'post',
     url: `${url}/${endpoint}`,
@@ -38,10 +36,12 @@ async function postApi(endpoint, username, password) {
     headers: { 
         'Content-Type': 'application/x-www-form-urlencoded',
         'accept': 'application/json',
+        // 'Authorization': `Bearer ${AUTH_TOKEN}`
+
     },
-    auth: {
-      'Authorization': `Bearer ${AUTH_TOKEN}`
-    }
+    // auth: {
+    //   'Authorization': `Bearer ${AUTH_TOKEN}`
+    // }
 
   })
   .then(function (res) {
@@ -55,6 +55,35 @@ async function postApi(endpoint, username, password) {
   });
 }
 
+async function getApi(endpoint, username, password) {
+
+  // TODO: write in solid colors. Authentication beaere token.
+  const AUTH_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJzdHJpbmciLCJleHAiOjE2NDk3MTkzMjl9.bseyHdgea7U0Dv_m8kJdjSEXCh1YFA32xBsOOgVbWnk'
+
+  await axios({
+    method: 'get',
+    url: `${url}/${endpoint}`,
+    data: { 'username': username, 'password': password },
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'accept': 'application/json',
+        'Authorization': `Bearer ${AUTH_TOKEN}`
+    },
+  })
+  .then(function (res) {
+    console.log({data: res.data})
+    console.log({status: res.status})
+    console.log('-------------------')
+    console.log({response: res})
+  })
+  .catch(function (err) {
+    // alert(error)
+    console.log({ ResData: err.response })
+    console.log({ StatusCode: err.response.status })
+  });
+}
+
 // ----- call -----
-getUsers()
+// getUsers()
+getApi('users/me/', 'string', 'string')
 // postApi('token', 'string', 'string')
