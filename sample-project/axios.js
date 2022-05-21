b1620d1e149ae7df17ecc22a342edd28a541f429
KazusaNakagawa <kazusanakagawa@gmail.com>
2022-05-21 14:45:42 +0900

    feat: POST login Bearer Authentication

--- sample-project/axios.js
+++ sample-project/axios.js
@@ -2,6 +2,7 @@
 // Ref: https://github.com/axios/axios
 
 const axios = require("axios")
+const FormData = require("form-data")
 const url = 'http://0.0.0.0:8000'
 // TODO: write in solid colors. Authentication beaere token.
 const AUTH_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJzdHJpbmciLCJleHAiOjE2NDk3NzQ3MDl9.FpUsHIraoc3q5AUqnE-bR2knTz0k22ZoGL067-b7yVQ'
@@ -26,35 +27,46 @@ async function getUsers() {
   });
 }
 
-async function postApi(endpoint, username, password) {
-  // axios.defaults.xsrfCookieName = 'csrftoken' // ←ココと
-  // axios.defaults.xsrfHeaderName = "X-CSRFTOKEN" // ←ココに追加しました
-  // axios.defaults.headers.common['Authorization'] = `Bearer ${AUTH_TOKEN}`
+
+async function postApi(endpoint, username_, password_) {
+  /** login
+   *
+   * 以下の curl は成功する
+   * $ curl -X 'POST' \
+   * 'http://localhost:8000/login/access-token' \
+   * -H 'accept: application/json' \
+   * -H 'Content-Type: application/x-www-form-urlencoded' \
+   * -d 'username=string&password=string'
+   *
+   * Resolved: https://github.com/axios/axios/issues/318
+   *
+   * ref:
+   *   https://fastapi-users.github.io/fastapi-users/usage/flow/
+   *   https://fastapi-users.github.io/fastapi-users/configuration/authentication/transports/bearer/
+  **/
+
+  const formData = new FormData()
+  formData.append('username', username_)
+  formData.append('password', password_)
+
+  // console.log(formData)
+
   await axios({
     method: 'post',
-    url: `${url}/${endpoint}`,
-    data: { 'username': username, 'password': password },
-    headers: { 
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'accept': 'application/json',
-        // 'Authorization': `Bearer ${AUTH_TOKEN}`
-
-    },
-    // auth: {
-    //   'Authorization': `Bearer ${AUTH_TOKEN}`
-    // }
-
+    url:`${url}/${endpoint}`,
+    data: formData,
+    headers: formData.getHeaders()
   })
   .then(function (res) {
     console.log({data: res.data})
     console.log({status: res.status})
   })
   .catch(function (err) {
-    // alert(error)
-    console.log({ ResData: err.response })
+    console.log({ Response: err.response })
     console.log({ StatusCode: err.response.status })
   });
 }
+
 
 async function getApi(endpoint, username, password) {
 
@@ -82,6 +94,6 @@ async function getApi(endpoint, username, password) {
 }
 
 // ----- call -----
-// getUsers()
+getUsers()
+postApi('login/access-token', 'test', 'test')
 // getApi('users/me/', 'string', 'string')
-postApi('token', 'string', 'string')
